d2b3bba1bd83a1582a46ba5ce9ef2c1caa21eb05
Angus Cheng <anguscheng389@gmail.com>
2023-01-24 12:40:23 +1000

    feat: added get_item function to retrieve items from a json file
    This retrieves an item from a specific case/collection, if present.

--- csgostashLoader.py
+++ csgostashLoader.py
@@ -1,31 +1,53 @@
 import json
-import pandas as pd
+
+rarities = {
+    "Mil-Spec Skins": 0.7992,
+    "Restricted Skins": 0.1598,
+    "Classified Skins": 0.0319,
+    "Covert Skins": 0.0063,
+    "Rare Special Items": 0.0025
+}
+
+extProb = {
+    "Battle-Scarred": 0.1054,
+    "Field-Tested": 0.3551,
+    "Minimal Wear": 0.2707,
+    "Factory New": 0.1549
+}
 
 
-def get_items():
+def get_item(item):
+    """
+    Retrieves item and its expected price
+    """
+    file = ('data/cases/json/chroma_2_case.json')
+    data = json.load(open(file))
 
-    rarities = {
-        "Mil-Spec Skins": 0.7992,
-        "Restricted Skins": 0.1598,
-        "Classified Skins": 0.0319,
-        "Covert Skins": 0.0063,
-        "Rare Special Items": 0.0025
-    }
-
-    with open('csgostash-scraper/data/cases/json/chroma_2_case.json', 'w') as read_file:
-        data = json.load(read_file)
-        df = pd.DataFrame(data)
-        dfAsCsv = df.to_csv(header=False, index=False)
-        # print(df.loc[:, 'content'])
-        # for rarity, prob in rarities.items():
-        #     for item in data['content'][rarity]:
-        #         items[item] = prob
-        #         print(item['name'])
-
-        # read_file.write(dfAsCsv)
-        json.dump(data, f)
-
-    return
+    for rarity in data["content"]:
+        for wep in data["content"][rarity]:
+            if wep["name"].startswith("â"):
+                knifeName = wep["name"][4:]
+                if knifeName == item:
+                    return wep
+            if wep["name"] == item:
+                return wep
 
 
-print(get_items())
+def get_rarity_prices(rarity):
+    """
+    Retrieves prices of a single rarity in a case
+    """
+    file = ('data/cases/json/chroma_2_case.json')
+    data = json.load(open(file))
+
+    item_prices = []
+    for item in data["content"]["Rare Special Items"]:
+        item_prices.append(item["prices"].get(rarity))
+        # price = float(price[3:])
+    print(item_prices)
+
+    return data
+
+
+# get_rarity_prices("Factory New")
+print(get_item_price("Karambit | Rust Coat"))
