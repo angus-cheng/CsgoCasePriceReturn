4b666bc4e363ae5677434377a6e1b5cf3417d65d
Angus Cheng <anguscheng389@gmail.com>
2023-01-24 19:28:29 +1000

    feat: get_item_expected_value() will return the expected value of an item from a case.
    This function will calculate the expected value of an item using the
    float proababilities.

--- csgostashLoader.py
+++ csgostashLoader.py
@@ -1,26 +1,12 @@
 import json
 
-rarities = {
-    "Mil-Spec Skins": 0.7992,
-    "Restricted Skins": 0.1598,
-    "Classified Skins": 0.0319,
-    "Covert Skins": 0.0063,
-    "Rare Special Items": 0.0025
-}
+from constants import souvProb, caseProb, floatProb, statTrakProb
 
-extProb = {
-    "Battle-Scarred": 0.1054,
-    "Field-Tested": 0.3551,
-    "Minimal Wear": 0.2707,
-    "Factory New": 0.1549
-}
-
-
-def get_item(item):
+def get_item(item, case):
     """
     Retrieves item and its expected price
     """
-    file = ('data/cases/json/chroma_2_case.json')
+    file = ("data/cases/json/" + case + "_case.json")
     data = json.load(open(file))
 
     for rarity in data["content"]:
@@ -32,22 +18,34 @@ def get_item(item):
             if wep["name"] == item:
                 return wep
 
+def get_item_expected_value(item, case):
+    wep = get_item(item, case)
+    itemPrices = wep["prices"]
+    extPrices = itemPrices
+    for ext in floatProb:
+        extPrice = float(itemPrices[ext][3:]) * floatProb.get(ext)
+        extPrices[ext] = extPrice
 
-def get_rarity_prices(rarity):
+    extPrices.pop("StatTrak")
+    expectedPrice = sum(extPrices.values())
+    return expectedPrice
+
+def get_rarity_prices(rarity, case):
     """
     Retrieves prices of a single rarity in a case
     """
-    file = ('data/cases/json/chroma_2_case.json')
+    file = ("data/cases/json/" + case + "_case.json")
     data = json.load(open(file))
 
     item_prices = []
-    for item in data["content"]["Rare Special Items"]:
-        item_prices.append(item["prices"].get(rarity))
+    for item in data["content"][rarity]:
+        item_prices.append(item["prices"])
         # price = float(price[3:])
     print(item_prices)
 
     return data
 
 
-# get_rarity_prices("Factory New")
-print(get_item_price("Karambit | Rust Coat"))
+# get_rarity_prices("Rare Special Items", "chroma_2")
+print(get_item_expected_value("Desert Eagle | Printstream", "fracture"))
+# print(get_item("Karambit | Rust Coat", "chroma_2"))
