6d02dfa51f64bb9b378bbe78384acf300d6dee6b
Angus Cheng <anguscheng389@gmail.com>
2023-01-24 19:30:19 +1000

    refractor: Pulled constants into a new constants.py file

---
+++ constants.py
@@ -0,0 +1,26 @@
+souvProb = {
+    "Consumer": 0.7992,
+    "Industrial": 0.1598,
+    "Mil-Spec": 0.032,
+    "Restricted": 0.0064,
+    "Classified": 0.00128,
+    "Covert": 0.000256
+}
+
+caseProb = {
+    "Mil-Spec": 0.7992,
+    "Restricted": 0.1598,
+    "Classified": 0.0319,
+    "Covert": 0.0063,
+    "Rare Special Items": 0.0025
+}
+
+floatProb = {
+    "Battle-Scarred": 0.16,
+    "Well-Worn": 0.24,
+    "Field-Tested": 0.33,
+    "Minimal Wear": 0.24,
+    "Factory New": 0.03
+}
+
+statTrakProb = 0.1
