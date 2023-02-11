868e9d1fc2917b923856b77ab6cb326af590ac98
Angus Cheng <anguscheng389@gmail.com>
2023-02-11 13:17:34 +1000

    Revert "test: test branch rebase"
    This reverts commit e7df9358d9a5c3fc7a133987de16aaf1c3fb3ae5.

--- csgostashLoader.py
+++ csgostashLoader.py
@@ -164,5 +164,9 @@ def get_rarity_prices(rarity, case):
 # print(get_item_expected_value("Desert Eagle | Corinthian", "Revolver"))
 # print(get_item("Karambit | Rust Coat", "chroma_2"))
 # print(calc_float_dist("Desert Eagle | Corinthian", "Revolver"))
+<<<<<<< HEAD
 print(calc_float_dist("Negev | Loudmouth", "Falchion"))
 # print(sum(calc_float_dist("Negev | Loudmouth", "Falchion").values()))
+=======
+print(calc_float_dist("Negev | Loudmouth", "Falchion"))
+>>>>>>> parent of e7df935 (test: test branch rebase)
